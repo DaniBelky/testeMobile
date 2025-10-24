1f7a4375188006aa128dcd4aad2ec864b7225fd9
DaniBelky <belchordaniel123@gmail.com>
2025-10-24 17:12:48 -0300

    começo do teste mobile project

---
+++ app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        MaterialButton btnCreate = findViewById(R.id.btnCreate);
+
+        btnCreate.setOnClickListener(v ->
+                Toast.makeText(this, "Conta criada com sucesso!", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
