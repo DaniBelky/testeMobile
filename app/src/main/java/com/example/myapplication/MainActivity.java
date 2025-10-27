96d22c20f86e66b83858ebca7cf2b3598b3ada33
DaniBelky <belchordaniel123@gmail.com>
2025-10-27 17:04:21 -0300

    tela de login

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,44 +9,10 @@ import com.google.android.material.button.MaterialButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private TextInputEditText etPassword, etConfirm;
-    private MaterialButton btnCreate;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Define o layout XML como conteúdo da tela
-        setContentView(R.layout.activity_signup);
-
-        etPassword = findViewById(R.id.etPassword);
-        etConfirm = findViewById(R.id.etConfirm);
-        btnCreate = findViewById(R.id.btnCreate);
-
-        btnCreate.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                validarSenhas();
-            }
-        });
+        setContentView(R.layout.activity_login);
     }
-
-    private void validarSenhas(){
-        String senha = etPassword.getText().toString();
-        String confirm = etConfirm.getText().toString();
-
-        if(senha.isEmpty() || confirm.isEmpty()){
-            Toast.makeText(this, "Preencha ambos os campos de senha", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if(!senha.equals(confirm)){
-            etConfirm.setError("As senhas não coincidem");
-            Toast.makeText(this, "As senhas não são iguais", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-
 }
--- app/src/main/java/com/example/myapplication/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -12,6 +12,6 @@ public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_login);
     }
 }
