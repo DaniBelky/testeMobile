a168511e82dbbd7cf0313788d5bd673d6c02fe84
DaniBelky <belchordaniel123@gmail.com>
2025-10-28 16:55:23 -0300

    tela menu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.myapplication;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-import com.google.android.material.textfield.TextInputEditText;
-import android.view.View;
-import com.google.android.material.button.MaterialButton;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Define o layout XML como conteúdo da tela
-        setContentView(R.layout.activity_login);
+
+        setContentView(R.layout.activity_menu);
+
+        TextView linkCadastro = findViewById(R.id.textView2);
+
+        linkCadastro.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
     }
 }
