0de22c57b47ec4c71bd57bd5f55a06519b057a2a
DaniBelky <belchordaniel123@gmail.com>
2025-11-04 16:55:09 -0300

    telas finalizadas

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView linkCadastro = findViewById(R.id.textViewLinkCadastro);
+
+        linkCadastro.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
+    }
+}
+
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -9,13 +10,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_menu);
 
-        TextView linkCadastro = findViewById(R.id.textView2);
+        Button loginButton = findViewById(R.id.loginButton);
 
-        linkCadastro.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+        loginButton.setOnClickListener(v ->{
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         });
     }
